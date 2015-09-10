f8b15f8225d203f6e5728b9fc3d537ae50e54f8a
William Butts <wbutts1023@gmail.com>
2015-09-09 21:14:37 -0400

    Got vector_mean working.  All tests pass. MVP.

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,3 +1,4 @@
+import math
 class ShapeException(Exception):
     pass
 
@@ -22,10 +23,6 @@ def vector_add(vector_1, vector_2):
     return [vector_1[i]+vector_2[i] for i in range(len(vector_1))]
 
 
-    # assert vector_add(v, w) == [1, 5, 4]
-    # assert vector_add(u, y) == [11, 21, 31]
-    # assert vector_add(u, z) == u
-
 def vector_sub(vector_1, vector_2):
     """
     [a b]  - [c d]  = [a-c b-d]
@@ -33,18 +30,15 @@ def vector_sub(vector_1, vector_2):
     Matrix + Matrix = Matrix
     """
     shape_exception_check(vector_1, vector_2)
-    return [vector_1[i] - vector_2[i] for i in len(vector_1)]
-    # assert vector_sub(v, w) == [1, 1, -4]
-    # assert vector_sub(w, v) == [-1, -1, 4]
-    # assert vector_sub(y, z) == y
-    # assert vector_sub(w, u) == vector_sub(z, vector_sub(u, w))
+    return [vector_1[i] - vector_2[i] for i in range(len(vector_1))]
+
 
 def vector_sum(*args):
-    return [vector_add(v_1,v_2) for v_1 in args for v_2 in args if v_1 != v_2]
-print(vector_sum([3,2,1],[2,1,3],[1,3,2],[1,2,3]))
-    # """vector_sum can take any number of vectors and add them together."""
-    #
-    # assert vector_sum(v, w, u, y, z) == [12, 26, 35]
+    base = [0]*len(args[0])
+    for arg in args:
+        base = vector_add(base, arg)
+    return base
+
 
 def dot(vector_1, vector_2):
     """
@@ -54,9 +48,6 @@ def dot(vector_1, vector_2):
     """
     shape_exception_check(vector_1, vector_2)
     return sum([vector_1[i]*vector_2[i] for i in range(len(vector_1))])
-    # assert dot(w, y) == 160
-    # assert dot(m, n) == 15
-    # assert dot(u, z) == 0
 
 
 def vector_multiply(vector, scalar):
@@ -66,15 +57,25 @@ def vector_multiply(vector, scalar):
     Vector * Scalar = Vector
     """
     return[vector[i]*scalar for i in range(len(vector))]
-    
-def test_vector_mean():
+
+
+def vector_mean(*args):
     """
     mean([a b], [c d]) = [mean(a, c) mean(b, d)]
 
-    mean(Vector)       = Vector
+    mean(Vector)= Vector
     """
-    assert vector_mean(m, n) == [4, 2]
-    assert vector_mean(v, w) == [0.5, 2.5, 2]
-    assert is_equal(vector_mean(v, w, u)[0], 2 / 3)
-    assert is_equal(vector_mean(v, w, u)[1], 2)
-    assert is_equal(vector_mean(v, w, u)[2], 5 / 3)
+    return [i/len(args) for i in vector_sum(*args)]
+
+
+def magnitude(vector):
+    """
+    magnitude([a b])  = sqrt(a^2 + b^2)
+
+    magnitude(Vector) = Scalar
+    """
+    return math.sqrt(sum([vector[i]**2 for i in range(len(vector))]))
+    assert magnitude(m) == 5
+    assert magnitude(v) == math.sqrt(10)
+    assert magnitude(y) == math.sqrt(1400)
+    assert magnitude(z) == 0
